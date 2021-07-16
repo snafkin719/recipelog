fc5707c340355056ca01f9f27d1467f4451b8f9d
EC2 Default User <ec2-user@ip-172-31-19-46.ap-northeast-1.compute.internal>
2021-07-16 03:46:26 +0000

    料理投稿機能

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   get 'users/index'
   get :signup,       to: 'users#new'
   resources :users
+  resources :dishes
   root 'static_pages#home'
   get :about,        to: 'static_pages#about'
   get :use_of_terms, to: 'static_pages#terms'
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_14_151033) do
+ActiveRecord::Schema.define(version: 2021_07_15_131439) do
+
+  create_table "dishes", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.float "portion"
+    t.text "tips"
+    t.text "reference"
+    t.integer "required_time"
+    t.integer "popularity"
+    t.text "cook_memo"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id", "created_at"], name: "index_dishes_on_user_id_and_created_at"
+    t.index ["user_id"], name: "index_dishes_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
